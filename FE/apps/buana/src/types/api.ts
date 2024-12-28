66a9ae1029d6846b0a42b6bd51c4b9a7cd353a3f
Farid Satria <faridsatria24@gmail.com>
2024-12-28 08:59:49 +0700

    clean code delete unused files

--- FE/apps/buana/src/types/api.ts
+++ FE/apps/buana/src/types/api.ts
@@ -4,9 +4,6 @@ export type ApiErrorResponse = {
 
 export enum ErrorEnum {
   INVALID = "INVALID_USERNAME_OR_PASSWORD",
-  InvalidDate = "INVALID_EVENT_DATE",
-  EventCategoryNameExists = "EVENT_CATEGORY_NAME_EXISTS",
-  EventCategoryRequired = "EVENT_CATEGORY_NOT_FOUND"
 }
 
 export type PaginatedParams = {
